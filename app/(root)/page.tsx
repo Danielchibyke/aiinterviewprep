eeda137e2c9d094311d53648a98cef1d5e30905a
Danielchibyke <danielchibyke5@gmail.com>
2025-03-27 13:09:05 +0100

    Setup app, implemented Sign-in and Login page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+function Home () {
+  return ( <>
+
+        <h1>MY NEXT JS</h1>
+  </> );
+}
+
+export default Home ;
---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import {  Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+ 
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "AI Interview Prep",
+  description: "An AI-Powered for preparing for mock interviews",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${monaSans.className} antialiased pattern`}
+      >
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
