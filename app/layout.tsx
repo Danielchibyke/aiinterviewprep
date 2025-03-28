94878dd746eee16c2e338b1ad58a8eaa5f0a9581
Danielchibyke <danielchibyke5@gmail.com>
2025-03-28 10:53:15 +0100

    Implemented the Home page and the Interview page  also integrating firebase and google gemini.

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${monaSans.className} antialiased pattern`}
+        className={`${monaSans.className} antialiased pattern dark`}
       >
         {children}
         <Toaster />
