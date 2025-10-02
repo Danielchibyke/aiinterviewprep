f8126c67a7fda38321baa4003f94c1185c6f274b
Danielchibyke <danielchibyke5@gmail.com>
2025-10-02 09:26:40 +0100

    changed to export output

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-SVXF5OsKXjJOlayu1epd7asN4_w3jXI",
-  authDomain: "ai-interviewer-22fce.firebaseapp.com",
-  projectId: "ai-interviewer-22fce",
-  storageBucket: "ai-interviewer-22fce.firebasestorage.app",
-  messagingSenderId: "967638897246",
-  appId: "1:967638897246:web:24a7c842a9ba2c6803dacf",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ,
   measurementId: "G-F2PVPBMEVL"
 };
 
